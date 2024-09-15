9a64efd9643bcde3acbcc181748aefca1a778fa3
吉野敬太郎 <yoshinoyukitarou@yoshinokeitarounoMacBook-Pro.local>
2024-09-15 14:54:42 +0900

    feat: markdown管理への移行用のセットアップを行った

---
+++ plopfile.js
@@ -0,0 +1,19 @@
+export default function (plop) {
+  const today = new Date();
+  const pathname = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}-${today.getMilliseconds()}`;
+
+  plop.setGenerator("blog", {
+    description: "blog post",
+    prompts: [],
+    actions: [
+      {
+        type: "add",
+        path: `src/pages/post/${pathname}/index.md`,
+        templateFile: "plop-templates/blog/index.md.hbs",
+        data: {
+          createdAt: today.toLocaleDateString(),
+        },
+      },
+    ],
+  });
+}
